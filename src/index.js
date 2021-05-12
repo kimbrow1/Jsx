3221d2ddcbe18884bb5e7e65ffd61f8832c543a9
Brian Kimbrow <bkimbrow25@gmail.com>
2021-05-12 17:50:18 -0500

    jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+ // import React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ // Create a react components
+const App =  () => {
+  const buttonText = { text: 'Click me' };
+  const labelText = 'Enter name';
+
+  return (
+     <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+        </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white',}}>
+        {buttonText.text}
+      </button>
+    </div>
+    );
+};
+
+
+
+ // Take the react components and show it on the screen
+
+ ReactDOM.render(
+      <App />,
+    document.querySelector('#root')
+ );
